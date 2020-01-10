8971c90266713afccb9e54e0959762af32ee52f3
KarryCMT <1570606465@qq.com>
2020-01-10 18:55:21 +0800

    enter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <MainTabBar></MainTabBar>
     <router-view/>
   </div>
 </template>
-
+<script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+export default {
+  name:'app',
+  components:{
+    MainTabBar,
+  }
+}
+</script>
 <style lang="less">
+@import '/assets/css/normalize.css';
+@import '/assets/css/base.css';
 </style>
